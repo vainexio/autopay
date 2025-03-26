24896d90288dd4795d9e21214d3dc69d56b2a8ba
Glitch (bot-template-11012) <none>
2025-03-26 13:06:31 +0000

    💝🌲 Checkpoint ./functions/qrGen.js:70636994/243

--- functions/qrGen.js
+++ functions/qrGen.js
@@ -37,7 +37,11 @@ function generateQrCRC(qrData) {
 
 module.exports = {
   generateQr: async function(amount) {
-    let qrData = "00020101021227830012com.p2pqrpay0111GXCHPHM2XXX02089996440303152170200000006560417DWQM4TK3JDO5YAWDV5204601653036085406"+amount.toFixed(2)+"5802PH5912I** PA**O I.6011San Antonio610412346304"
+    let digitIndicator = amount >= 1000 ? "7"
+    : amount >= 100 ? "6"
+    : amount >= 10 ? "5"
+    : amount >= 1 ? "4" : "Unknown"
+    let qrData = "00020101021227830012com.p2pqrpay0111GXCHPHM2XXX02089996440303152170200000006560417DWQM4TK3JDO5YAWDV520460165303608540"+digitIndicator+amount.toFixed(2)+"5802PH5912I** PA**O I.6011San Antonio610412346304"
     let generatedQr = generateQrCRC(qrData)
     
     let data = {
