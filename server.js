f24c976c7d47e9c3161c1a103313b4a30134f5b1
Glitch (bot-template-11012) <none>
2025-04-16 13:20:13 +0000

    🐮⚽️ Checkpoint ./server.js:70636994/208

--- server.js
+++ server.js
@@ -408,7 +408,7 @@ client.on("interactionCreate", async (inter) => {
       let qrCode = await generateQr(amount,"For "+(inter.user.username),true) //inter.member.nickname ? inter.member.nickname : 
       console.log(qrCode)
       try {
-        const background = await Jimp.read('https://cdn.glitch.global/ef5aba0e-2698-4d9a-9dfb-7c60e08418a2/Logopit_1744808520090.png?v=1744808561298');
+        const background = await Jimp.read('https://cdn.glitch.global/ef5aba0e-2698-4d9a-9dfb-7c60e08418a2/Logopit_1744808813428.png?v=1744808880344');
         const qrLink = await Jimp.read(qrCode.image);
 
         const newWidth = background.bitmap.width / 2;
