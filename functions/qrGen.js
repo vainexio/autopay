78a144834d30c4705436b8884d3e6785bc94cfb8
vai <111634400+vainexio@users.noreply.github.com>
2025-07-25 18:48:14 +0800

    Update qrGen.js

--- functions/qrGen.js
+++ functions/qrGen.js
@@ -65,7 +65,7 @@ function generateQrCRC(qrData) {
 }
 
 module.exports = {
-  generateQr: async function (amount,name,plain) {
+  generateQr: async function (amount,name,plain,platform) {
     name = cutString(name.replace('ω',''))
     console.log(name)
     let digitIndicator =
@@ -76,8 +76,11 @@ module.exports = {
       : amount >= 1 ? "5404"
       : "Unknown";
     let initial = "01021" + (amount > 0 ? "2" : "1")
-    let qrData = "000201"+initial+"27780012com.p2pqrpay0111PAPHPHM1XXX02089996440304126394532635490515+63-945-3263549520460165303608"+(Number(amount) > 0 ? digitIndicator + Number(amount).toFixed(2) : "")+"5802PH59"+formatNumber(name.length)+name+"6015Sto. Tomas City6304"
-    let qrData2 = "000201"+initial+"27830012com.p2pqrpay0111GXCHPHM2XXX02089996440303152170200000006560417DWQM4TK3JDO7SRDOU5204601653036085"+digitIndicator+amount.toFixed(2)+"5802PH59"+formatNumber(name.length)+name+"6011San Antonio610412346304";
+    let qrData
+    let maya = "000201"+initial+"27780012com.p2pqrpay0111PAPHPHM1XXX02089996440304126394532635490515+63-945-3263549520460165303608"+(Number(amount) > 0 ? digitIndicator + Number(amount).toFixed(2) : "")+"5802PH59"+formatNumber(name.length)+name+"6015Sto. Tomas City6304"
+    let gcash = "000201"+initial+"27830012com.p2pqrpay0111GXCHPHM2XXX02089996440303152170200000006560417DWQM4TK3JDO7SRDOU5204601653036085"+digitIndicator+amount.toFixed(2)+"5802PH59"+formatNumber(name.length)+name+"6011San Antonio610412346304";
+    platform == "gcash" ? qrData = gcash :
+    platform == "maya" ? qrData = maya : null
     let generatedQr = generateQrCRC(qrData);
     let dynamicDesign = {"body":"circular","eye":"frame1","eyeBall":"ball0","erf1":["fh"],"erf2":[],"erf3":["fh","fv"],"brf1":[],"brf2":[],"brf3":[],"bodyColor":"#382600","bgColor":"#FFFFFF","eye1Color":"#382600","eye2Color":"#382600","eye3Color":"#382600","eyeBall1Color":"#382600","eyeBall2Color":"#382600","eyeBall3Color":"#382600","gradientColor1":"","gradientColor2":"","gradientType":"linear","gradientOnEyes":"true","logo":"","logoMode":"default"}
     let plainDesign = {"body":"square","eye":"frame2","eyeBall":"ball0","erf1":["fv"],"erf2":[],"erf3":[],"brf1":[],"brf2":[],"brf3":[],"bodyColor":"#000000","bgColor":"#FFFFFF","eye1Color":"#000000","eye2Color":"#000000","eye3Color":"#000000","eyeBall1Color":"#000000","eyeBall2Color":"#000000","eyeBall3Color":"#000000","gradientColor1":"","gradientColor2":"","gradientType":"linear","gradientOnEyes":"true","logo":"https://cdn.glitch.global/ef5aba0e-2698-4d9a-9dfb-7c60e08418a2/pio.png","logoMode":"default"}
